6ad89c7c9c28c4cacd074233c2542e4124b7f826
Zack Braksa <zakaria.braksa@gmail.com>
2023-09-24 21:28:44 +0100

    Add new projects

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -14,13 +14,24 @@ const projects: Project[] = [
   {
     title: "Startup Patterns",
     techs: ["Bite-sized startup lessons for busy founders."],
-    link: "https://www.producthunt.com/products/startup-patterns",
+    link: "https://www.producthunt.com/products/startup-patterns#startup-patterns",
   },
   {
     title: "Hillo for Trello",
     techs: ["Board activity tracking for Trello."],
-    link: "https://www.producthunt.com/products/hillo",
+    link: "https://www.producthunt.com/products/hillo#hillo",
   },
+  {
+    title: "Hidden Founders",
+    techs: ["Build your MVP on a bootstrap-friendly payment plan."],
+    link: "https://www.producthunt.com/products/hidden-founders?comment=57857#hidden-founders",
+  },
+  {
+    title: "Qudos",
+    techs: ["Product Hunt for dev tool"],
+    link: "https://www.producthunt.com/products/qudos#qudos"
+  }
+
 ];
 
 export default projects;
